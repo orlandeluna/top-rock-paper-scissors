4f7c4e3480aab1060f8b324ec1b70d41d7c58691
Orlando <orlanmoon@gmail.com>
2022-12-01 11:12:38 -0600

    Add the .js file

---
+++ script.js
@@ -0,0 +1,6 @@
+let weaponOptions = ["Rock", "Paper", "Scissors"];
+    let weaponChoice = weaponOptions[Math.floor(Math.random()*weaponOptions.lenght)];
+
+    function getComputerChoice(){
+      
+    }
